75b880e1527a02cae6026e99cb32e9c0f555864f
ckode <ckode@d2629fb4-2503-43e5-876f-338d16625718>
2012-04-15 03:43:03 +0000

    * Forgot to add the new schema sql file for classes to last commit * Added GPL3 license to maps.sql

---
+++ data/ArenaMUD2.sql
@@ -0,0 +1,40 @@
+--  ArenaMUD2 - A multiplayer combat game - http://arenamud.david-c-brown.com
+--  Copyright (C) 2012 - David C Brown & Mark Richardson
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+CREATE TABLE classes ( 
+    name                 VARCHAR( 15 )   PRIMARY KEY
+                                         NOT NULL,
+    attacks              INTEGER         NOT NULL,
+    attackskill          INTEGER         NOT NULL
+                                         CHECK ( attackskill > 0 
+                                             AND
+                                         attackskill < 100 ) 
+                                         DEFAULT ( 50 ),
+    maxdamage            INT             NOT NULL,
+    mindamage            INT             NOT NULL,
+    powerdesc            VARCHAR( 10 )   NOT NULL,
+    maxhp                INTEGER         NOT NULL,
+    stealth              BOOLEAN         NOT NULL
+                                         DEFAULT ( 0 ),
+    weapontextyouhit     VARCHAR( 100 )  NOT NULL,
+    weapontextvictimhit  VARCHAR( 100 )  NOT NULL,
+    weapontextroomhit    VARCHAR( 100 )  NOT NULL,
+    classdesc            VARCHAR( 512 )  NOT NULL,
+    weapontextyoumiss    VARCHAR( 100 )  NOT NULL,
+    weapontextvictimmiss VARCHAR( 100 )  NOT NULL,
+    weapontextroommiss   VARCHAR( 100 )  NOT NULL 
+);
